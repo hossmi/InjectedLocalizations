04f16a2956aa6236f54537d353f752d993d00b28
Pablo Jimena <hossmi@gmail.com>
2023-03-04 19:10:35 +0100

    Add AbstractLocalizations abstract class for from client interfaces generated types

---
+++ InjectedLocalizations.Abstractions/Abstractions/AbstractLocalizations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace InjectedLocalizations.Abstractions
+{
+    public abstract class AbstractLocalizations { }
+
+    public abstract class AbstractLocalizations<T> : AbstractLocalizations, ILocalizations where T : ILocalizations
+    {
+        protected T instance;
+
+        public AbstractLocalizations(IServiceProvider serviceProvider, IDictionary<CultureInfo, Type> localizationTypes)
+        {
+            this.instance = (T)serviceProvider.CreateInstance(localizationTypes);
+        }
+
+        public CultureInfo Culture => this.instance.Culture;
+
+    }
+}
---
+++ InjectedLocalizations.Abstractions/Abstractions/AbstractLocalizationsExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using InjectedLocalizations.Configuration;
+using InjectedLocalizations.Exceptions;
+using InjectedLocalizations.Providers;
+
+namespace InjectedLocalizations.Abstractions
+{
+    public static class AbstractLocalizationsExtensions
+    {
+        public static object CreateInstance(this IServiceProvider serviceProvider, IDictionary<CultureInfo, Type> localizationTypes)
+        {
+            object instance;
+            ILocalizationsCultureConfiguration cultureConfiguration;
+            ICurrentCultureProvider currentCultureProvider;
+            ConstructorInfo defaultConstructor;
+
+            serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+            localizationTypes = localizationTypes ?? throw new ArgumentNullException(nameof(localizationTypes));
+            cultureConfiguration = (ILocalizationsCultureConfiguration)serviceProvider.GetService(typeof(ILocalizationsCultureConfiguration));
+            currentCultureProvider = (ICurrentCultureProvider)serviceProvider.GetService(typeof(ICurrentCultureProvider));
+
+            if (!currentCultureProvider.TryGetCurrent(out CultureInfo currentCulture))
+                currentCulture = cultureConfiguration.Default;
+
+            if (!localizationTypes.TryGetValue(currentCulture, out Type concreteType))
+                throw new NotFoundConcreteTypeForSelectedCultureLocalizationException(currentCulture);
+
+            defaultConstructor = concreteType
+                .GetConstructor(new Type[0])
+                ?? throw new ConstructorNotFoundLocalizationException(concreteType, new Type[0]);
+
+            instance = defaultConstructor.Invoke(new object[0]);
+
+            return instance;
+        }
+    }
+}
