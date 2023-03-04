9d4f3930c25d28dc561d3cfeafd164e980e07887
Pablo Jimena <hossmi@gmail.com>
2023-03-04 21:16:28 +0100

    Add DefaultLocalizationsProvider project

---
+++ InjectedLocalizations.Providers.DefaultLocalizationsProvider/Providers/DefaultLocalizationsProvider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using InjectedLocalizations.Building;
+using InjectedLocalizations.Exceptions;
+using InjectedLocalizations.MemberParsing;
+using JimenaTools.Extensions.Strings;
+
+namespace InjectedLocalizations.Providers
+{
+    public class DefaultLocalizationsProvider : AbstractLocalizationsProvider
+    {
+        public override bool CanIssueToWriters => true;
+        public override bool CanWriteFromIssuers => false;
+
+        public override void Write(ILocalizationResponse response) => throw new NotWritableProviderLocalizationException(this);
+
+        protected override IReadOnlyDictionary<MemberInfo, string> GetLocalizationsOrNull(ILocalizationRequest request)
+        {
+            return request
+                .InterfaceType
+                .GetMethodsAndProperties()
+                .Where(m => !m.IsILocalizationsCultureProperty())
+                .Select(MemberParserExtensions.AsParsedMember)
+                .Select(parsed => new
+                {
+                    parsed.Member,
+                    Value = parsed
+                        .AsInterpolatedString()
+                        .AsJoined(),
+                })
+                .ToDictionary(m => m.Member, m => m.Value);
+        }
+    }
+}
---
+++ InjectedLocalizations.Providers.DefaultLocalizationsProvider/Providers/DefaultLocalizationsProviderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using InjectedLocalizations.MemberParsing;
+using InjectedLocalizations.MemberParsing.Tokens;
+
+namespace InjectedLocalizations.Providers
+{
+    public static class DefaultLocalizationsProviderExtensions
+    {
+        public static IEnumerable<string> AsInterpolatedString(this IParsedMember parsed)
+        {
+            foreach (IToken token in parsed)
+            {
+                if (!(token is IPrintableToken printable))
+                    continue;
+
+                if (printable is ParameterToken parameter)
+                {
+                    yield return "{";
+                    yield return parameter.Value;
+                    yield return "}";
+                    continue;
+                }
+
+                yield return printable.Value;
+            }
+        }
+    }
+}
